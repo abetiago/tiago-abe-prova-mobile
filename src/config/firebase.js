ae84c99419da9ddcf717d9d3967b1cc31dcaa848
abetiago <56887416+abetiago@users.noreply.github.com>
2020-07-03 00:23:27 -0300

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,14 +2,14 @@ import * as firebase from 'firebase';
 import 'firebase/firestore';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBlDRG0mfCg4ynSjZ2bdETid-8EIp3QxE8",
-    authDomain: "prova-c36da.firebaseapp.com",
-    databaseURL: "https://prova-c36da.firebaseio.com",
-    projectId: "prova-c36da",
-    storageBucket: "prova-c36da.appspot.com",
+    apiKey: "EOcbRuM3TGH8kjWq7gnQjZ2poETid-6as54s65d,
+    authDomain: "mobile-b48ea.firebaseapp.com",
+    databaseURL: "https://mobile-c36da.firebaseio.com",
+    projectId: "mobile-c36da",
+    storageBucket: "mobile-c36da.appspot.com",
     messagingSenderId: "1095774557497",
-    appId: "1:1095774557497:web:c74872198d2957330e04e8",
-    measurementId: "G-BJSCFT8XP9"
+    appId: "1:1523648963125:web:c74872198d2957330e04e8",
+    measurementId: "H-KIJNGT7CP9"
 };
 
 // Initialize Firebase
